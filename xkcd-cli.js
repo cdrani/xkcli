35b4334ca53bfec66eec3d97f636831b06ef8a9e
cdrainxv <cdrainxv@users.noreply.github.com>
2017-12-04 15:14:00 -0700

    add previous, next options

--- xkcd-cli.js
+++ xkcd-cli.js
@@ -8,21 +8,36 @@ const store = require('data-store')('xkcd')
 program
   .version('0.1.0')
   .option('-c, --current', 'most current comic')
+  .option('-p, --previous', 'previous comic from current')
+  .option('-n, --next', 'next comic from current')
   .option('-s, --specific <value>', 'specific comic')
   .parse(process.argv)
 
 async function fetchComic(url) {
   const response = await fetch(url)
   const data = await response.json()
-  const { alt, img, title } = data
-    console.log(chalk.bold.underline.blueBright(title))
-    imgcat(img, { log: true })
-    console.log(chalk.greenBright(alt))
+  const { num, alt, img, title } = data
+  store
+    .set('current', num)
+    .set('previous', num - 1)
+    .set('next', num + 1)
+
+  console.log(chalk.bold.underline.blueBright(title))
+  imgcat(img, { log: true })
+  console.log(chalk.greenBright(alt))
 }
 
-switch(process.argv[2]) {
+switch (process.argv[2]) {
   case '-c': {
     return fetchComic(`https://xkcd.com/info.0.json`)
+  }
+
+  case '-p': {
+    return fetchComic(`https://xkcd.com/${store.get('previous')}/info.0.json`)
+  }
+
+  case '-n': {
+    return fetchComic(`https://xkcd.com/${store.get('next')}/info.0.json`)
   }
 
   case '-s': {
