82fb9c444d8bde35c42559879150aea98a416c1b
cdrainxv <cdrainxv@users.noreply.github.com>
2019-01-27 14:31:25 -0700

    refactor from use of options to sub commands

--- index.js
+++ index.js
@@ -1,92 +1,97 @@
 #!/usr/bin/env node
-const program = require('commander')
-const fetch = require('node-fetch')
-const chalk = require('chalk')
-const imgcat = require('imgcat')
-const store = require('data-store')('xkcd')
-const rand = require('unique-random')
+const program = require("commander");
+const fetch = require("node-fetch");
+const chalk = require("chalk");
+const imgcat = require("imgcat");
+const store = require("data-store")("xkcd");
+const rand = require("unique-random");
 
-const list = chalk_opts => chalk_opts.split(',')
+const BASE_URL = "https://xkcd.com";
+const SUFFIX = "info.0.json";
+
 const setOptions = (opts = `bold.greenBright`, type, text) =>
-  store.get(type)
-    ? console.log(chalk`{${opts} ${text}}`)
-    : console.log(chalk.bold.greenBright(text))
+	store.get(type)
+		? console.log(chalk`{${opts} ${text}}`)
+		: console.log(chalk.bold.greenBright(text));
+
+const fetchComic = async url => {
+	const response = await fetch(url);
+	const data = await response.json();
+	const { num, alt, img, title } = data;
+	store.set("current", num);
+
+	let latest = store.get("latest") || store.get("current");
+
+	if (program.current) {
+		store.set("latest", num);
+	}
+
+	const isLatest = num === store.get("latest");
+
+	store
+		.set("previous", num <= 1 ? 1 : num - 1)
+		.set("next", isLatest ? num : num + 1);
+
+	setOptions(store.get("title"), "title", `${title} [${num}/${latest}]`);
+	imgcat(img).then(image => {
+		console.log(image);
+		setOptions(store.get("alt"), "alt", alt);
+	});
+};
+
+
+program.version("0.3.2", "-v, --version");
 
 program
-  .version('0.3.2')
-  .option('-c, --current', 'latest comic')
-  .option('-p, --previous', 'previous comic based on the present comic')
-  .option('-n, --next', 'next comic based on the present comic')
-  .option(
-    '-s, --specific <value>',
-    'specific comic ranging from first to latest'
-  )
-  .option('-r, --random', 'random comic ranging from first to latest')
-  .option(
-    '-o, --options <options>',
-    'configure chalk options (ref. chalk package)',
-    list
-  )
-  .option('-d, --data', 'display filepath to data storage location')
-  .parse(process.argv)
+	.command("c")
+	.description("display the latest comic")
+	.action(() => fetchComic(`${BASE_URL}/${SUFFIX}`));
 
-async function fetchComic(url) {
-  const response = await fetch(url)
-  const data = await response.json()
-  const { num, alt, img, title } = data
-  store.set('current', num)
+program
+	.command("p")
+	.description("display the previous comic based on the present comic")
+	.action(() => fetchComic(`${BASE_URL}/${store.get("previous")}/${SUFFIX}`));
 
-  let latest = store.get('latest') || store.get('current')
+program
+	.command("n")
+	.description("display the next comic based on the present comic")
+	.action(() => fetchComic(`${BASE_URL}/${store.get("next")}/${SUFFIX}`));
 
-  if (program.current) {
-    store.set('latest', num)
-  }
+program
+	.command("s <value>")
+	.description("display specific comic ranging from first to latest")
+	.action(val => {
+		const latest = store.get("latest");
+		if (val < 1 || val > latest) {
+			return console.log(
+				`Only comics ranging from 1 to ${latest} can be retrieved.`
+			);
+		}
+		return fetchComic(`${BASE_URL}/${val}/${SUFFIX}`);
+	});
 
-  const isLatest = num === store.get('latest')
+program
+	.command("r")
+	.description("display random comic ranging from first to latest")
+	.action(() =>
+		fetchComic(`${BASE_URL}/${rand(1, store.get("latest"))()}/${SUFFIX}`)
+	);
 
-  store
-    .set('previous', num <= 1 ? 1 : num - 1)
-    .set('next', isLatest ? num : num + 1)
+program
+	.command("o <options>")
+	.description(
+		"configure chalk options to change color of output text (ref chalk package)"
+	)
+	.action(chalkOpts => {
+		const [titleStyle, altStyle] = chalkOpts.split(",");
+		return store.set("title", titleStyle).set("alt", altStyle);
+	});
 
-  setOptions(store.get('title'), 'title', `${title} [${num}/${latest}]`)
-  imgcat(img).then(image => {
-    console.log(image)
-    setOptions(store.get('alt'), 'alt', alt)
-  })
-}
+program
+	.command("d")
+	.description("display filepath to data storage location")
+	.action(() => console.log(store.path));
 
-switch (process.argv[2]) {
-  case '-c':
-    return fetchComic(`https://xkcd.com/info.0.json`)
+program.parse(process.argv);
 
-  case '-p':
-    return fetchComic(`https://xkcd.com/${store.get('previous')}/info.0.json`)
-
-  case '-n':
-    return fetchComic(`https://xkcd.com/${store.get('next')}/info.0.json`)
-
-  case '-s':
-    const latest = store.get('latest')
-    const specificNum = program.specific
-    if (specificNum < 1 || specificNum > latest) {
-      return console.log(
-        `Only comics ranging from 1 to ${latest} can be retrieved.`
-      )
-    }
-    return fetchComic(`https://xkcd.com/${specificNum}/info.0.json`)
-
-  case '-r':
-    return fetchComic(
-      `https://xkcd.com/${rand(1, store.get('latest'))()}/info.0.json`
-    )
-
-  case '-o':
-    return store.set('title', program.options[0]).set('alt', program.options[1])
-
-  case '-d':
-    return console.log(store.path)
-
-  default:
-    console.log('For available commands/options, reference `xk -h`')
-    process.exit(1)
-}
+if (!program.args.length) program.help();
